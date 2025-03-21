26267b2e52abaa75fb0be38bc6e90ade07bf80ed
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-03-22 00:13:52 +0600

    basic js

---
+++ js/modul.js
@@ -0,0 +1,43 @@
+// let name="mosaraf";
+// name='name change';
+// console.log(name)
+
+// const age=30;
+// const name="mosaraf";
+// if(name==="mosaraf" && age<=20){
+//     console.log("kotha sotto");
+// }
+// else{
+//     console.log("kotha misa")
+// }
+
+//  const frinds=['mosaraf', "rony",'imran', 'ahad'];
+
+//  for(let i=0;i<frinds.length;i++){
+//     console.log(frinds[i]);
+//  }
+
+// frinds.push("goni")
+// frinds.pop()
+// frinds.shift()
+// frinds.unshift('ami')
+//  console.log(frinds)
+
+// function sum(a,b){
+
+//     console.log(...arguments)
+//     console.log(arguments.length)
+//     return a+b
+// }
+
+// const result=sum(2,3)
+// console.log(result)
+
+persons={
+    name:"hero",
+    age:23,
+    frinds:['ami asi', 'tmi '],
+    status:true
+}
+
+console.log(persons.age)
