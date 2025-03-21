34e7269699c5b2715a439b8a08b5ca9e9c384136
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-03-22 00:49:41 +0600

    spread operator

--- js/modul.js
+++ js/modul.js
@@ -33,11 +33,39 @@
 // const result=sum(2,3)
 // console.log(result)
 
-persons={
-    name:"hero",
-    age:23,
-    frinds:['ami asi', 'tmi '],
-    status:true
-}
+// persons={
+//     name:"hero",
+//     age:23,
+//     frinds:['ami asi', 'tmi '],
+//     status:true
+// }
 
-console.log(persons.age)
+// console.log(persons.age)
+
+
+// const fName="mosaraf";
+// const lName="Hossen";
+// console.log(` hi ami ${fName} ${lName}`)
+
+
+// const sum =(a,b)=>a+b;
+// const result=sum(12,8)
+// console.log(result)
+
+
+// const sum=(...rest)=>{
+//     // console.log(rest)
+//     console.log(rest.length)
+
+//     for(let i=0;i<rest.length;i++){
+//         console.log(rest[i])
+//     }
+// }
+
+
+const numbers=[12,2,3,1,3,2]
+const numr=[98,35,65];
+const add=[...numbers, ...numr]
+console.log(add)
+
+// console.log(...numbers)
